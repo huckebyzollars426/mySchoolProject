47f548a3b4e52d0bb01bdbc53336d33d598bf535
huckebyzollars426 <huckebyzollars426@gmail.com>
2025-04-05 05:04:21 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,8 @@
+def calculate_total_score(scores):
+    total = sum(scores)
+    return total
+
+scores = [85, 92, 76, 90, 88]
+total_score = calculate_total_score(scores)
+
+print("Total score:", total_score)
