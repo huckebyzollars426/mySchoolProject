a67eb2f3536d49a01c50727852632bf0b93682ad
huckebyzollars426 <huckebyzollars426@gmail.com>
2025-04-07 14:35:11 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,12 @@
+def my_function(param1):
+    """
+    This is an example function that accepts one parameter.
+    
+    Parameters:
+    param1 (any): The value of the parameter to be passed into the function.
+    
+    Returns:
+    str: A message containing the result of the function execution.
+    """
+    # Function implementation goes here
+    return f"The value of 'param1' is {param1}."
