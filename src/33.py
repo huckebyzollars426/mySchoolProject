648e8ffe58252ddd7b5c01c4c13c4e8693c46f76
huckebyzollars426 <huckebyzollars426@gmail.com>
2025-04-25 23:15:41 +0000

    Add 33.py

---
+++ src/33.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_square_root(num):
+    """
+    Calculate and return the square root of the given number.
+    
+    Args:
+    num (float or int): The number to calculate the square root of.
+    
+    Returns:
+    float: The square root of the given number.
+    """
+    return math.sqrt(num)
+
+# Example usage
+result = calculate_square_root(16)
+print(result)  # Output will be approximately 4.0
